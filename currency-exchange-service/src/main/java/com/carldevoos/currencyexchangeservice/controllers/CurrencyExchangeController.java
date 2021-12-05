20a169d1c0be115c20ec0eda9158154f5803b364
carldevoos <carl.devoos@gmail.com>
2021-12-04 21:12:12 -0500

    Add repository JPA

--- currency-exchange-service/src/main/java/com/carldevoos/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/carldevoos/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -1,13 +1,12 @@
 package com.carldevoos.currencyexchangeservice.controllers;
 
 import com.carldevoos.currencyexchangeservice.models.CurrencyExchange;
+import com.carldevoos.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
@@ -15,12 +14,21 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieverExchangeValue(
             @PathVariable String from,
             @PathVariable String to
     ) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(4.1));
+        /*CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(4.1));
+         */
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for ".concat(from).concat(" to ").concat(to));
+        }
 
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
