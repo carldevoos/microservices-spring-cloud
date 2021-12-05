9c597f6aede8109bc07a5647010d9c3ec90c39e8
carldevoos <carl.devoos@gmail.com>
2021-12-04 20:57:31 -0500

    Add JPA

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into currency_exchange
+(currency_from, currency_to, conversion_multiple, environment)
+values ('USD', 'PEN', 4.1, '');
+
+insert into currency_exchange
+(currency_from, currency_to, conversion_multiple, environment)
+values ('PEN', 'USD', 0.25, '');
+
+insert into currency_exchange
+(currency_from, currency_to, conversion_multiple, environment)
+values ('EUR', 'PEN', 4.61, '');
+
+insert into currency_exchange
+(currency_from, currency_to, conversion_multiple, environment)
+values ('PEN', 'EUR', 0.22, '');
+
+insert into currency_exchange
+(currency_from, currency_to, conversion_multiple, environment)
+values ('USD', 'EUR', 0.88, '');
+
+insert into currency_exchange
+(currency_from, currency_to, conversion_multiple, environment)
+values ('EUR', 'USD', 1.33, '');
