f9d06dbf9270dd3e94ba52f4cbe962118140542d
carldevoos <carl.devoos@gmail.com>
2021-12-05 22:44:10 -0500

    Add currency-conversion-service

---
+++ currency-conversion-service/src/main/java/com/carldevoos/currencyconversionservice/proxys/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.carldevoos.currencyconversionservice.proxys;
+
+import com.carldevoos.currencyconversionservice.models.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversion retrieverExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to);
+
+}
