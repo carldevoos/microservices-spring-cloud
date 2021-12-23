5decc9c390f10ae2d299ddf9c55727239640c69d
carldevoos <carl.devoos@gmail.com>
2021-12-22 19:18:09 -0500

    currency-exhange-service docker 0.0.11

--- currency-exchange-service/src/main/java/com/carldevoos/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/carldevoos/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.carldevoos.currencyexchangeservice.controllers;
 
 import com.carldevoos.currencyexchangeservice.models.CurrencyExchange;
 import com.carldevoos.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +19,16 @@ public class CurrencyExchangeController {
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieverExchangeValue(
             @PathVariable String from,
-            @PathVariable String to
-    ) {
+            @PathVariable String to) {
         /*CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(4.1));
          */
+        logger.info("retrieverExchangeValue, from {} to {}", from, to);
+
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
 
         if (currencyExchange == null) {
@@ -31,7 +36,12 @@ public class CurrencyExchangeController {
         }
 
         String port = environment.getProperty("local.server.port");
-        currencyExchange.setEnvironment(port);
+
+        // Change for kubernetes
+        String host = environment.getProperty("HOSTNAME");
+        String version = "v11";
+
+        currencyExchange.setEnvironment(String.format("%s %s %s",port, version, host));
 
         return currencyExchange;
     }
